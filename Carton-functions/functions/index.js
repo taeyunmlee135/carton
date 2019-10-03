7831ade820a0d8fa44f9e9a1fd5e6965de381468
rebeccaqin <rebeccafqin@gmail.com>
2019-10-02 22:06:31 -0700

    rewrote w/o using express

--- Carton-functions/functions/index.js
+++ Carton-functions/functions/index.js
@@ -1,9 +1,41 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const app = require('express')();
+
 admin.initializeApp();
 
+exports.getChores = functions.https.onRequest((request, response) => {
+    admin.firestore().collection('Chores').get()
+        .then(data => {
+            let chores = [];
+            data.forEach(doc => {
+                chores.push(doc.data());
+            });
+            return response.json(chores);
+        })
+        .catch(err => console.error(err));
+});
+
+exports.createChore = functions.https.onRequest((request, response) => {
+    const newChore = {
+        chore : req.body.chore,
+        userSubmitted : req.body.userSubmitted, 
+        userDo : req.body.userDo,
+        postedAt : admin.firestore.Timestamp.fromDate(new Date())
+    };
+
+    admin.firestore().collection('Chores').add(newChore)
+    .then(doc => {
+        response.json({ message: `document ${doc.id} created successfully`});
+    })
+    .catch(err => {
+        response.status(500).json({ error: 'something went wrong'});
+        console.error(err);
+    });
+});
+
+/*
+const app = require('express')();
 const firebaseConfig = {
     apiKey: "AIzaSyCntBiECLjGFAA5Z8ynh_mJUbBfXVNLkGw",
     authDomain: "carton-5d613.firebaseapp.com",
@@ -85,3 +117,4 @@ app.post('/signup', (req, res) => {
     });
 });
  exports.api = functions.https.onRequest(app);
+ */
