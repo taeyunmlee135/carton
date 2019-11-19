168b5b88943a5981b4337f7ecf83f86b252bb2f9
rebeccaqin <rebeccafqin@gmail.com>
2019-11-18 21:06:59 -0800

    working on integrating gcal api

--- Carton-functions/functions/index.js
+++ Carton-functions/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const app = require('express')();
+const {google} = require('googleapis');
 
 admin.initializeApp(); 
 
@@ -50,5 +51,80 @@ app.post("/chores", (req, res, next) => {
     });
 });
 
+// Google Calendar API stuff
+function getCredentials() {
+  return {"web":{"client_id":"499457247630-7gmg3e4if4af690nt1klo3h66sfbsasd.apps.googleusercontent.com",
+  "project_id":"carton-5d613",
+  "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+  "token_uri":"https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+  "client_secret":"ib0T1NL7uSKjY4LnFKJZn336",
+  "javascript_origins":["https://us-central1-carton-5d613.cloudfunctions.net"]}}
+  //Replace this empty object with credentials.json contents
+}
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+exports.authorize = functions.https.onRequest((request, response) => {
+    console.log('Authorize called')
+
+    const {client_secret, client_id, redirect_uris} = getCredentials().web
+    if(!client_secret || !client_id || !redirect_uris)
+        response.send("Credentials missing").status(500)
+
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
+    let url = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    })
+    response.send(url)
+})
+
+
+exports.token = functions.https.onRequest((request, response) => {
+    console.log('token called ' + JSON.stringify(request.body))
+    const code = request && request.query && request.query.code || null
+    if(!code)
+        response.send("code missing").status(400)
+
+    const {client_secret, client_id, redirect_uris} = getCredentials().web
+    if(!client_secret || !client_id || !redirect_uris)
+        response.send("Credentials missing").status(500)
+
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0])
+    oAuth2Client.getToken(code, (err, token) => {
+        if(err) {
+            console.log('err ' + err.message)
+            response.status(500).send("Unable to generate token " + err.message)
+        }
+        oAuth2Client.setCredentials(token)
+
+        fetchEvents(oAuth2Client)
+        response.send("Access Granted. Please close this tab and continue.")
+    })
+})
+
+
+function fetchEvents(auth) {
+    const calendar = google.calendar({version: 'v3', auth})
+    calendar.events.list({
+        calendarId: 'primary',
+        timeMin: (new Date()).toISOString(),
+        maxResults: 10,
+        singleEvents: true,
+        orderBy: 'startTime',
+      }, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err);
+        const events = res.data.items;
+        if (events.length) {
+          console.log('Upcoming 10 events:');
+          events.map((event, i) => {
+            const start = event.start.dateTime || event.start.date;
+            console.log(`${start} - ${event.summary}`);
+          });
+        } else {
+          console.log('No upcoming events found.');
+        }
+    })
+}
 
 exports.api = functions.region('us-central1').https.onRequest(app); // name must match with firebase.json
