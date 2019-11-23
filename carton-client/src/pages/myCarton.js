46e1e9280bc374fa9870a737c76ce8cd83c67074
Taeyun Lee <talee@hmc.edu>
2019-11-23 10:21:19 -0800

    added new page myCarton for users to view their carton

---
+++ carton-client/src/pages/myCarton.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import chores from './chores';
+
+export class mycarton extends Component {
+
+
+
+
+}
+
+export default mycarton;
