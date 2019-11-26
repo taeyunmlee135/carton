c80e904d5094fc9126ae2fe1ee0a59a37d57859f
Taeyun Lee <talee@hmc.edu>
2019-11-25 22:16:18 -0800

    commented out buggy AuthRoute for easier testing

--- carton-client/src/App.js
+++ carton-client/src/App.js
@@ -56,12 +56,11 @@ class App extends Component {
                     <Route exact path = '/addChores' component = {addChores} />
 
                     {/* if authenticated, login and signup will just redirect to homepage */}
-                    <AuthRoute exact path = '/login' component = {login} authenticated={authenticated}/>
-                    <AuthRoute exact path = '/signup' component = {signup} authenticated={authenticated}/>
+                    {/* <AuthRoute exact path = '/login' component = {login} authenticated={authenticated}/>
+                    <AuthRoute exact path = '/signup' component = {signup} authenticated={authenticated}/> */}
 
-                    {/* without already-authenticated -> redirect check
+                    {/* without already-authenticated -> redirect check */}
                     <Route exact path = '/login' component = {login} />
-                    <Route exact path = '/signup' component = {signup} /> */}
                     <Route exact path = '/signup' component = {signup} />
                     <Route exact path = '/groceries' component = {groceries} />
                     <Route exact path = '/addGroceries' component = {addGroceries} />
