4477981c47b03fdc1fa9a89e30f9441a9c9a5e69
Taeyun Lee <talee@hmc.edu>
2019-12-06 12:01:49 -0800

    preliminary div structures for homepage

--- carton-client/src/pages/home.js
+++ carton-client/src/pages/home.js
@@ -1,18 +1,31 @@
 import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid';
+import "./home.css";
+import profilePicture from '../images/egg.png';
+
 
 export class home extends Component {
 
     render(){
         return (
-            <Grid container spacing={16}>
-                <Grid item sm={8} xs={12}>
-                    <p>Household/Carton</p>
-                </Grid>
-                <Grid item sm={4} xs={12}>
-                    <p>Profile</p>
-                </Grid>
-            </Grid>
+            
+            <div> 
+            <h2> announcements! </h2>
+                <div id="hAnnounce"> 
+                    <div className="uAnnouncement" > 
+                        <h3> <img src={profilePicture} width={'10%'}/>    monica </h3> 
+                        <p> package arriving at 4pm today! </p> 
+                    </div> 
+                    <div className="uAnnouncement" > 
+                        <h3> <img src={profilePicture} width={'10%'}/> taeyun </h3>
+                        <p> plumber coming at 2pm 12/7 </p>
+                    </div> 
+                    <div className="uAnnouncement" > 
+                        <h3> <img src={profilePicture} width={'10%'}/> rebecca </h3>
+                        <p> extra chocolate chip cookies on the kitchen counter! </p> 
+                    </div> 
+                </div> 
+            </div> 
         )
     }
 
