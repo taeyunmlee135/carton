d74c03888b3ee9c95582ba0d6bef96b2c428bd11
Taeyun Lee <talee@hmc.edu>
2019-12-06 14:28:26 -0800

    preliminary structures of homepage

--- carton-client/src/pages/home.js
+++ carton-client/src/pages/home.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
+
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+
+import { Link } from 'react-router-dom';
+
+
+
 import "./home.css";
 import profilePicture from '../images/egg.png';
 
@@ -8,23 +15,64 @@ export class home extends Component {
 
     render(){
         return (
-            
-            <div> 
-            <h2> announcements! </h2>
-                <div id="hAnnounce"> 
-                    <div className="uAnnouncement" > 
-                        <h3> <img src={profilePicture} width={'10%'}/>    monica </h3> 
-                        <p> package arriving at 4pm today! </p> 
+            <div className="wrapper"> 
+            <div id="announcements"> 
+                <h2> announcements! </h2>
+                    <div id="hAnnounce"> 
+                        <div className="uAnnouncement" > 
+                            <h3> <img src={profilePicture} width={'10%'}/>    monica </h3> 
+                            <p> package arriving at 4pm today! </p> 
+                        </div> 
+                        <div className="uAnnouncement" > 
+                            <h3> <img src={profilePicture} width={'10%'}/> taeyun </h3>
+                            <p> plumber coming at 2pm 12/7 </p>
+                        </div> 
+                        <div className="uAnnouncement" > 
+                            <h3> <img src={profilePicture} width={'10%'}/> rebecca </h3>
+                            <p> extra chocolate chip cookies on the kitchen counter! </p> 
+                        </div> 
                     </div> 
-                    <div className="uAnnouncement" > 
-                        <h3> <img src={profilePicture} width={'10%'}/> taeyun </h3>
-                        <p> plumber coming at 2pm 12/7 </p>
-                    </div> 
-                    <div className="uAnnouncement" > 
-                        <h3> <img src={profilePicture} width={'10%'}/> rebecca </h3>
-                        <p> extra chocolate chip cookies on the kitchen counter! </p> 
-                    </div> 
+                <Button component={Link} to="/mycarton" >
+                view all 
+                </Button>
+            </div> 
+
+            <div class="twocol">
+                <div className="previewBox" id="chorePreview"> 
+                    <h2> chores! </h2>
+                        <div id="hChores"> 
+                            <div className="cChore" > 
+                                <p> clean </p> 
+                            </div> 
+                            <div className="cChore" > 
+                                <p> clean </p> 
+                            </div> 
+                            <div className="cChore" > 
+                                <p> clean </p> 
+                            </div> 
+                        </div> 
+                    <Button component={Link} to="/chores" >
+                    view all 
+                    </Button>
                 </div> 
+                <div className="previewBox" id="shoppingPreview">
+                <h2> shopping! </h2>
+                    <div id="hShop"> 
+                        <div className="cShop" > 
+                            <p> clean </p> 
+                        </div> 
+                        <div className="cShop" > 
+                            <p> clean </p> 
+                        </div> 
+                        <div className="cShop" > 
+                            <p> clean </p> 
+                        </div> 
+                    </div> 
+                    <Button component={Link} to="/groceries" >
+                    view all 
+                    </Button>
+                </div>
+            </div> 
             </div> 
         )
     }
